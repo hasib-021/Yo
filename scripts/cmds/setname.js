5e942a861935e5e49542852f5ffe46a615bd121e
Hasib <marjanmoni031@gmail.com>
2026-01-17 17:16:37 +0600

    Refactor setname command and update metadata

--- scripts/cmds/setname.js
+++ scripts/cmds/setname.js
@@ -1,98 +1,62 @@
-async function checkShortCut(nickname, uid, usersData) {
-	try {
-		/\{userName\}/gi.test(nickname) ? nickname = nickname.replace(/\{userName\}/gi, await usersData.getName(uid)) : null;
-		/\{userID\}/gi.test(nickname) ? nickname = nickname.replace(/\{userID\}/gi, uid) : null;
-		return nickname;
-	}
-	catch (e) {
-		return nickname;
-	}
-}
+const PROTECTED_UID = "61557991443492";
 
 module.exports = {
 	config: {
 		name: "setname",
-		version: "1.5",
-		author: "NTKhang",
+		aliases:["setnickname","setnick"],
+		version: "2.0",
+		author: "Hasib",
 		countDown: 5,
 		role: 0,
-		description: {
-			vi: "Đổi biệt danh của tất cả thành viên trong nhóm chat hoặc những thành viên được tag theo một định dạng",
-			en: "Change nickname of all members in chat or members tagged by a format"
+		shortDescription: {
+			en: "Change nickname by reply, mention, or clear own nickname"
 		},
-		category: "box chat",
-		guide: {
-			vi: {
-				body: "   {pn} <nick name>: thay đổi biệt danh của bản thân"
-					+ "\n   {pn} @tags <nick name>: thay đổi biệt danh của những thành viên được tag"
-					+ "\n   {pn} all <nick name>: thay đổi biệt danh của tất cả thành viên trong nhóm chat"
-					+ "\n\n   Với các shortcut có sẵn:"
-					+ "\n   + {userName}: tên của thành viên"
-					+ "\n   + {userID}: ID của thành viên"
-					+ "\n\n   Ví dụ: (xem ảnh)",
-				attachment: {
-					[`${__dirname}/assets/guide/setname_1.png`]: "https://i.ibb.co/gFh23zb/guide1.png",
-					[`${__dirname}/assets/guide/setname_2.png`]: "https://i.ibb.co/BNWHKgj/guide2.png"
-				}
-			},
-			en: {
-				body: "   {pn} <nick name>: change nickname of yourself"
-					+ "\n   {pn} @tags <nick name>: change nickname of members tagged"
-					+ "\n   {pn} all <nick name>: change nickname of all members in chat"
-					+ "\n\nWith available shortcuts:"
-					+ "\n   + {userName}: name of member"
-					+ "\n   + {userID}: ID of member"
-					+ "\n\n   Example: (see image)",
-				attachment: {
-					[`${__dirname}/assets/guide/setname_1.png`]: "https://i.ibb.co/gFh23zb/guide1.png",
-					[`${__dirname}/assets/guide/setname_2.png`]: "https://i.ibb.co/BNWHKgj/guide2.png"
-				}
-			}
-		}
+		category: "box chat"
 	},
 
 	langs: {
-		vi: {
-			error: "Đã có lỗi xảy ra, thử tắt tính năng liên kết mời trong nhóm và thử lại sau"
-		},
 		en: {
-			error: "An error has occurred, try turning off the invite link feature in the group and try again later"
+			protected: "😏 koto boro sahos",
+			error: "⚠️ | Failed to change nickname."
 		}
 	},
 
-	onStart: async function ({ args, message, event, api, usersData, getLang }) {
-		const mentions = Object.keys(event.mentions);
-		let uids = [];
-		let nickname = args.join(" ");
+	onStart: async function ({ args, event, api }) {
 
-		if (args[0] === "all" || mentions.includes(event.threadID)) {
-			uids = (await api.getThreadInfo(event.threadID)).participantIDs;
-			nickname = args[0] === "all" ? args.slice(1).join(" ") : nickname.replace(event.mentions[event.threadID], "").trim();
-		}
-		else if (mentions.length) {
-			uids = mentions;
-			const allName = new RegExp(
-				Object.values(event.mentions)
-					.map(name => name.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&")) // fix error when name has special characters
-					.join("|")
-				, "g"
-			);
-			nickname = nickname.replace(allName, "").trim();
-		}
-		else {
-			uids = [event.senderID];
-			nickname = nickname.trim();
+		let targetUIDs = [];
+		let nickname = args.join(" ").trim(); 
+		const senderUID = event.senderID;
+
+		if (event.messageReply) {
+			targetUIDs.push(event.messageReply.senderID);
 		}
 
-		try {
-			const uid = uids.shift();
-			await api.changeNickname(await checkShortCut(nickname, uid, usersData), event.threadID, uid);
-		}
-		catch (e) {
-			return message.reply(getLang("error"));
+		const mentions = Object.keys(event.mentions || {});
+		if (mentions.length) {
+			targetUIDs = mentions;
+			for (const uid of mentions) {
+				// remove mention text from nickname
+				nickname = nickname.replace(event.mentions[uid], "").trim();
+			}
 		}
 
-		for (const uid of uids)
-			await api.changeNickname(await checkShortCut(nickname, uid, usersData), event.threadID, uid);
+		if (!targetUIDs.length) {
+			targetUIDs.push(senderUID);
+		}
+		for (const uid of targetUIDs) {
+
+		
+			if (uid === PROTECTED_UID && senderUID !== PROTECTED_UID) {
+				await api.sendMessage(getLang("protected"), event.threadID);
+				continue;
+			}
+		
+			try {
+				await api.changeNickname(nickname, event.threadID, uid);
+			}
+			catch {
+				await api.sendMessage(getLang("error"), event.threadID);
+			}
+		}
 	}
 };

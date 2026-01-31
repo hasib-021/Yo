e3d3211a0de85d8c8a79143a43542d75709d93b6
Hasib <marjanmoni031@gmail.com>
2026-02-01 03:22:46 +0600

    Create bmdp.js

---
+++ scripts/cmds/bmdp.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+
+module.exports = {
+  config: {
+    name: "bmdp",
+    aliases: ["boysmatchingdp" , "bdp"],
+    version: "1.0",
+    author: "Hasib",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Random Boys Matching DP",
+    longDescription: "Send random Boys Matching DP",
+    category: "image",
+    guide: "{pn}"
+  },
+
+  onStart: async function ({ api, event }) {
+    try {
+      const res = await axios.get("https://xsaim8x-xxx-api.onrender.com/api/bmdp");
+      const { boy, boy2 } = res.data;
+
+      api.sendMessage(
+        {
+          body: "🥰✨️",
+          attachment: await Promise.all([
+            global.utils.getStreamFromURL(boy),
+            global.utils.getStreamFromURL(boy2)
+          ])
+        },
+        event.threadID,
+        event.messageID
+      );
+    } catch (e) {
+      api.sendMessage("❌ Couldn't fetch BMDP.", event.threadID, event.messageID);
+      console.error(e);
+    }
+  }
+};

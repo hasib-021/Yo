6322264a8a89604b55c0cf1bacaa08633b6c88cf
Hasib <marjanmoni031@gmail.com>
2026-02-10 10:09:31 +0600

    Update admin.js

--- scripts/cmds/admin.js
+++ scripts/cmds/admin.js
@@ -3,7 +3,7 @@ const { writeFileSync } = require("fs-extra");
 
 // --- Owner setup ---
 const OWNER_ID = ["61587417024496", "61583864293558", "61557991443492"];
-const OWNER_DISPLAY_NAME = "🅺🅰🆁🅸🅼 🅱🅴🅽🆰🅸🅼🅰";
+const OWNER_DISPLAY_NAME = "🅺🅰🆁🅸🅼 🅱🅴🅽🆉🅸🅼🅰";
 
 module.exports = {
   config: {
